7ed5e1d80e00ab30371774cf5532c17363841015
Morgan Cyphers <morganlcyphers@gmail.com>
2017-07-24 22:04:18 +0000

    Lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrolled_for_current_course, only: [:show]
+
 	def show 
 	end
 
 
 	private
+
+	def require_enrolled_for_current_course
+    if current_lesson.section.course != current_user
+    	redirect_to course_path(current_lesson.section.course), alert: "You are not enrolled for this course."
+    end
+  end
 
 	helper_method :current_lesson
 	def current_lesson
