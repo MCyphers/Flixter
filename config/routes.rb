1698b3680d1ae52b493aa0168dd3e1ce1c663e87
Morgan Cyphers <morganlcyphers@gmail.com>
2017-08-08 21:28:44 +0000

    Lesson 36

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
 	namespace :instructor do
     resources :sections, only: [:update]
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
-        resources :lessons, only: [:new, :create]
+    resources :sections, only: [:update] do
+        resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
